39434f120d72ee29336cdd3bada8b73a1ce8ed14
MyUsername <my@email.com>
2023-01-10 16:49:12 +0100

    create all section for card but not responsive

--- src/components/AppCardMain.vue
+++ src/components/AppCardMain.vue
@@ -1,17 +1,33 @@
 
 <script>
 export default {
-   
+   name: 'AppCardMain',
+   props: [
+            'imageUrl',
+            'filmPrice',
+            'filmName',
+            'filmType'
+      ]
 }
 </script>
 
 <template lang="">
-   <div>
-      
+   <div class="card-box">
+      <img :src="imageUrl" :alt="filmName">
+      <h3>{{ filmName }}</h3>
    </div>
 </template>
 
 
 <style lang="scss">
-   
+
+   .card{
+      width: calc((100% / 6))
+      
+
+      img{
+         width: 250px;
+         height: 250px;
+      }
+   }
 </style>
